14b72a5716b3710031eb487ca03f563c491fb248
Derek Calabro <dtcalabro@gmail.com>
2021-09-03 13:02:32 -0400

    setup

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+/*
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -12,5 +13,19 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+*/
 
-//console.log('TEST');
+
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
